344a5d867181b28bfbd8f0d95992bbea4054c982
gbenga504 <daveanifowoshe@ymail.com>
2018-08-09 16:16:13 +0100

    Add the back button effect to app

--- src/screens/InceptionScreen.js
+++ src/screens/InceptionScreen.js
@@ -1,17 +1,37 @@
 import React from "react";
 import { AsyncStorage } from "react-native";
+import { NavigationActions } from "react-navigation";
 
 export default class InceptionScreen extends React.PureComponent {
   componentDidMount() {
-    let {
-      navigation: { navigate }
-    } = this.props;
+    let { navigation } = this.props;
 
     AsyncStorage.getItem("@userType", (error, type) => {
       if (error || !type) {
-        navigate("selectAccount");
+        navigation.dispatch(
+          NavigationActions.reset({
+            index: 0,
+            key: null,
+            actions: [
+              NavigationActions.navigate({
+                routeName: "selectAccount"
+              })
+            ]
+          })
+        );
       } else {
-        navigate("dashboard", { type });
+        navigation.dispatch(
+          NavigationActions.reset({
+            index: 0,
+            key: null,
+            actions: [
+              NavigationActions.navigate({
+                routeName: "dashboard",
+                params: { type }
+              })
+            ]
+          })
+        );
       }
     });
   }
